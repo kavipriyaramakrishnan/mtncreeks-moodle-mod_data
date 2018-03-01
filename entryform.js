66ac43bb17f14bf9c57834c3a34fd81d47165e03
Pukunui <support@pukunui.com>
2018-03-01 14:28:32 +0800

    Committing the changes done to the core mod_data for TXE-274165

---
+++ entryform.js
@@ -0,0 +1,28 @@
+M.mod_data = {};
+alert('here');
+
+M.mod_data.init_view = function(Y) {alert('in');
+    Y.io("entryform.php", {
+    method: "POST",
+    data: "value=1",
+    on:{
+        success: function(data, result) {
+        console.log(result.responseText);
+        var vals = [];
+        try {
+          vals = Y.JSON.parse(result.responseText);
+        } 
+        catch (e) {
+          alert("JSON Parse failed");
+          return;
+        }
+        var node = Y.one("#field_"+vals.id);
+        node.set('value', vals.name);
+        console.log('success');
+        },
+        failure: function(id, result) {
+        }
+    }
+    });
+};
+
